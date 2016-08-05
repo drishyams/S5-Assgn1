963c3c807a1d2b51962a95a077376346beb030c9
Drishya MS <drishya@Drishyas-MacBook-Air.local>
2016-08-05 12:55:33 -0700

    S5-Assn1

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcCircleArea(rad){
+	var area = Math.PI * rad * rad ;
+	return area;
+}
+
+document.write("Area of the circle with radiis 7 = " + calcCircleArea(7) + "<br><br>");
+document.write("Area of the circle with radiis 1.5 = " +calcCircleArea(1.5) + "<br><br>");
+document.write("Area of the circle with radiis 20 = " +calcCircleArea(20) + "<br><br>");
+
